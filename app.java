046c9a0fa079575739462935836fa1c6df79a91b
KarriBhavyaSri <bhavyasrikarri370@gmail.com>
2026-02-12 12:09:08 +0530

    2 Update app.java

--- app.java
+++ app.java
@@ -25,3 +25,9 @@ public class Main {
       System.out.println(num + " is not a prime number.");
   }
 }
+
+//nextTerm = firstTerm + secondTerm; (0 + 1)
+firstTerm = secondTerm; (1)
+secondTerm = nextTerm; (1)
+
+nextTerm = firstTerm + secondTerm; (1 + 1)
